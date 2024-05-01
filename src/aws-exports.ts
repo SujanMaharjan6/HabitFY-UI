25c5bd9ace63ff79225afcb81ecca27635960048
Sujan Maharjan <smaharjan6@myseneca.ca>
2024-05-01 15:54:40 -0400

    update aws stuffs

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -1,11 +1,11 @@
 const awsconfig = {
   Auth: {
-    region: 'us-east-1',
-    userPoolId: 'us-east-1_4HifTZrM4',
-    userPoolWebClientId: '309nvlfov3ioandke2pippaj17',
+    region: '',
+    userPoolId: '',
+    userPoolWebClientId: '',
     oauth: {
-      domain: 'habitfy-app.auth.us-east-1.amazoncognito.com',
-      scope: ['aws.cognito.signin.user.admin', 'email', 'openid', 'phone', 'profile'],
+      domain: '',
+      scope: [],
       redirectSignIn: 'https://habitfy.vercel.app/home',
       redirectSignOut: 'https://habitfy.vercel.app',
       responseType: 'code'
